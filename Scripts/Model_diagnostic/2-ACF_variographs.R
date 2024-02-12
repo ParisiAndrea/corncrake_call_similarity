fc55799e6ef5e9927dfdb4c63015e1d1f9c41f43
ANDREA PARISI - RESEARCH <andrea9561@live.it>
2024-02-12 16:11:50 +0000

    edits

---
+++ Scripts/Model_diagnostic/2-ACF_variographs.R
@@ -0,0 +1,49 @@
+####Intra-season model
+bacf1 <- acf(na.omit(lag(residuals(mod1))), plot = FALSE)
+bacfdf1 <- with(bacf1, data.frame(lag, acf))
+
+durbinWatsonTest(mod1)
+
+a1 = ggplot(bacfdf1, aes(x=lag, y=acf)) +
+  geom_bar(stat = "identity", position = "identity", fill = '#14907B') +
+  annotate(geom = 'label', 
+           x=12,y=0.95,
+           label.size = NA,
+           label = 'D-W=1.89, P=0.53') +
+  scale_y_continuous(name = 'ACF',
+                     limits = c(-0.4,1),
+                     breaks = c(-0.4,-0.2,0,0.2,0.4,0.6,0.8,1)) +
+  xlab('LAG') +
+  theme_pubclean()
+
+print(a1)
+
+####Between-year model
+bacf2 <- acf(na.omit(lag(residuals(mod2))), plot = FALSE)
+bacfdf2 <- with(bacf2, data.frame(lag, acf))
+
+durbinWatsonTest(mod2)
+
+a2 = ggplot(bacfdf2, aes(x=lag, y=acf)) +
+  geom_bar(stat = "identity", position = "identity", fill = '#14907B') +
+  annotate(geom = 'label', 
+           x=12,y=0.95,
+           label.size = NA,
+           label = 'D-W=2.11, P=0.91') +
+  scale_y_continuous(name = '',
+                     limits = c(-0.4,1),
+                     breaks = c(-0.4,-0.2,0,0.2,0.4,0.6,0.8,1)) +
+  xlab('LAG') +
+  theme_pubclean()
+
+print(a2)
+
+ggsave('acf.jpg',
+       ggarrange(a1,a2,
+                 nrow=1,
+                 labels = c('A','B')),
+       path = 'C:/Users/G00399072/OneDrive - Atlantic TU/Desktop/call_paper/graphs',
+       width = 200, height = 100, units = "mm",
+       dpi = 600)
+
+###END
