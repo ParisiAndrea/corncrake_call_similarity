09d100229816319f4b6e256d8f41afcf6fc3010b
ANDREA PARISI - RESEARCH <andrea9561@live.it>
2024-02-12 16:22:32 +0000

    new scripts

---
+++ Scripts/Extra_figures/1-Map.R
@@ -0,0 +1,168 @@
+#load packages
+sapply(c('data.table', 'dplyr', 'tidyr','ggplot2',
+         'rgdal','rgeos','maptools','tmap','ggpubr',
+         'terra','ggsn','broom','maps','grid'), 
+       require, 
+       character.only=T)
+
+p = ggplot() + coord_fixed() +
+  xlab("") + ylab("")
+
+
+###CORNCRAKE LOCATIONS
+recs = readOGR('C:/Users/G00399072/OneDrive - Atlantic TU/Desktop/call_paper/shp/corncrake_locs.shp')
+
+recs = spTransform(recs, CRS('EPSG: 29902')) %>% 
+  as.data.table() %>% 
+  rename(long = coords.x1,lat = coords.x2)
+
+recs = fortify(recs)
+
+###IRELAND
+ire_shp = readOGR('C:/Users/G00399072/OneDrive - Atlantic TU/Desktop/ecoacoustics_paper/shp/ireland.shp')
+
+ire_shp = spTransform(ire_shp, CRS('EPSG: 29902'))
+
+ire_shp = fortify(ire_shp)
+
+ire <- p + geom_polygon(data=ire_shp, aes(x=long, y=lat, group=group), 
+                        colour='grey40', fill="grey40") +
+  geom_rect(aes(ymax = 2.7e+5, ymin = 2.5e+5, xmin = .5e+5, xmax = .6e+5), fill = NA, color = '#FAAB36', linewidth = 1) +
+  geom_rect(aes(ymax = 3.45e+5, ymin = 3.15e+5, xmin = .6e+5, xmax = .75e+5), fill = NA, color = '#FAAB36', linewidth = 1) +
+  geom_rect(aes(ymax = 4.45e+5, ymin = 4.3e+5, xmin = 1.85e+5, xmax = 2.1e+5), fill = NA, color = '#FAAB36', linewidth = 1) +
+  geom_rect(aes(ymax = 4.6e+5, ymin = 4.45e+5, xmin = 2.3e+5, xmax = 2.45e+5), fill = NA, color = '#FAAB36', linewidth = 1) +
+  annotate("text", x=.4e+5, y=2.7e+5, label= "D", fontface =2, size = 8) +
+  annotate("text", x=.5e+5, y=3.45e+5, label= "C", fontface =2, size = 8) +
+  annotate("text", x=1.75e+5, y=4.45e+5, label= "B", fontface =2, size = 8) +
+  annotate("text", x=2.2e+5, y=4.6e+5, label= "A", fontface =2, size = 8) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+        panel.background = element_rect(fill = 'white', colour = 'white'), 
+        axis.line = element_line(colour = "white"), legend.position="none",
+        axis.ticks=element_blank(), axis.text.x=element_blank(),
+        axis.text.y=element_blank()) +
+  theme_void() +
+  ggsn::scalebar(ire_shp, dist_unit = 'km', dist = 50, transform = F, 
+                 st.dist = 0.02, st.size=4, height=0.02, location = 'bottomright') +
+  ggsn::north(data = ire_shp, symbol=3)
+
+ire
+
+###MULLET
+mul_shp = readOGR('C:/Users/G00399072/OneDrive - Atlantic TU/Desktop/ecoacoustics_paper/shp/mullet.shp')
+
+mul_shp = spTransform(mul_shp, CRS('EPSG: 29902'))
+
+mul_shp = fortify(mul_shp)
+
+mul <- p + 
+  geom_polygon(data=mul_shp, aes(x=long, y=lat, group=group), 
+               colour='grey40', fill="grey40") +
+  geom_point(data=recs[population == 'mullet'], 
+             aes(x=long, y=lat), fill = '#FAAB36', colour="black", pch=21, size=4, position = position_jitter()) +
+  ggsn::scalebar(mul_shp, dist_unit = 'km', dist = 2, transform = F, 
+                 st.dist = 0.04, st.size=3, height=0.02, location = 'bottomright') +
+  theme_void() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+        panel.background = element_rect(fill = 'white', colour = 'white'), 
+        axis.line = element_line(colour = "white"), legend.position="none",
+        axis.ticks=element_blank(), axis.text.x=element_blank(),
+        axis.text.y=element_blank())
+
+mul
+
+##GALWAY
+gal_shp = readOGR('C:/Users/G00399072/OneDrive - Atlantic TU/Desktop/call_paper/shp/galway.shp')
+
+gal_shp = spTransform(gal_shp, CRS('EPSG: 29902'))
+
+gal_shp = fortify(gal_shp)
+
+gal <- p + 
+  geom_polygon(data=gal_shp, aes(x=long, y=lat, group=group), 
+               colour='grey40', fill="grey40") +
+  geom_point(data=recs[population == 'galway'], 
+             aes(x=long, y=lat), fill = '#FAAB36', colour="black", pch=21, size=4, position = position_jitter()) +
+  ggsn::scalebar(gal_shp, dist_unit = 'km', dist = 1, transform = F, 
+                 st.dist = 0.04, st.size=3, height=0.02, location = 'bottomright') +
+  theme_void() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+        panel.background = element_rect(fill = 'white', colour = 'white'), 
+        axis.line = element_line(colour = "white"), legend.position="none",
+        axis.ticks=element_blank(), axis.text.x=element_blank(),
+        axis.text.y=element_blank())
+
+gal
+
+
+##W_DONEGAL
+wdo_shp = readOGR('C:/Users/G00399072/OneDrive - Atlantic TU/Desktop/call_paper/shp/w_donegal.shp')
+
+wdo_shp = spTransform(wdo_shp, CRS('EPSG: 29902'))
+
+wdo_shp = fortify(wdo_shp)
+
+wdo <- p + 
+  geom_polygon(data=wdo_shp, aes(x=long, y=lat, group=group), 
+               colour='grey40', fill="grey40") +
+  geom_point(data=recs[population == 'w_donegal'], 
+             aes(x=long, y=lat), fill = '#FAAB36', colour="black", pch=21, size=4, position = position_jitter()) +
+  ggsn::scalebar(wdo_shp, dist_unit = 'km', dist = 2, transform = F,
+                 st.dist = 0.05, st.size=3, height=0.03, location = 'bottomright') +
+  theme_void() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+        panel.background = element_rect(fill = 'white', colour = 'white'), 
+        axis.line = element_line(colour = "white"), legend.position="none",
+        axis.ticks=element_blank(), axis.text.x=element_blank(),
+        axis.text.y=element_blank())
+
+wdo
+
+
+###E_DONEGAL
+##W_DONEGAL
+edo_shp = readOGR('C:/Users/G00399072/OneDrive - Atlantic TU/Desktop/call_paper/shp/e_donegal.shp')
+
+edo_shp = spTransform(edo_shp, CRS('EPSG: 29902'))
+
+edo_shp = fortify(edo_shp)
+
+edo <- p + 
+  geom_polygon(data=edo_shp, aes(x=long, y=lat, group=group), 
+               colour='grey40', fill="grey40") +
+  geom_point(data=recs[population == 'e_donegal'], 
+             aes(x=long, y=lat), fill = '#FAAB36', colour="black", pch=21, size=4, position = position_jitter()) +
+  ggsn::scalebar(edo_shp, dist_unit = 'km', dist = 1, transform = F,
+                 st.dist = 0.04, st.size=3, height=0.02, location = 'bottomright') +
+  theme_void() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+        panel.background = element_rect(fill = 'white', colour = 'white'), 
+        axis.line = element_line(colour = "white"), legend.position="none",
+        axis.ticks=element_blank(), axis.text.x=element_blank(),
+        axis.text.y=element_blank())
+
+edo
+
+#COMBINE
+mp = ggarrange(ggarrange(edo,
+                         wdo,
+                         mul,
+                         gal,
+                         ncol = 2, nrow = 2,
+                         labels = c('A','B','C','D'),
+                         font.label = list(size = 20)),
+               ire,
+               ncol = 2)
+
+print(mp)
+
+#SAVE
+ggsave('map.jpg',
+       mp,
+       path = 'C:/Users/G00399072/OneDrive - Atlantic TU/Desktop/call_paper/graphs',
+       width = 300,
+       height = 180,
+       units = 'mm',
+       dpi = 600
+)
+
+###END
---
+++ Scripts/Extra_figures/2-Waveforms.R
@@ -0,0 +1,146 @@
+#load packages
+sapply(c('data.table','dplyr',
+         'ggplot2','ggpubr',
+         'stringr','tidyr',
+         'seewave','tuneR',
+         'patchwork'), 
+       require, 
+       character.only=T)
+
+
+#CREATE DATASETS
+####
+wav1 <-  readWave('C:/Users/G00399072/OneDrive - Atlantic TU/Desktop/call_paper/ZOOM0018.WAV_cleaned.wav',
+                  from = 9.8, to = 16, units = 'seconds') %>% channel('right')
+
+####
+wav2 <-  readWave('C:/Users/G00399072/OneDrive - Atlantic TU/Desktop/call_paper/ZOOM0018.WAV_cleaned.wav',
+                  from = 10.1, to = 10.3, units = 'seconds') %>% channel('left')
+
+####
+wav3 <-  readWave('C:/Users/G00399072/OneDrive - Atlantic TU/Desktop/call_paper/ZOOM0018.WAV_cleaned.wav',
+                  from = 10.113, to = 10.16, units = 'seconds')  %>% channel('left')
+
+### PLOTTING
+g1 = ggplot(data.frame(amp = wav1@left,
+                       time = seq_along(wav1@left)/10^5)) +
+  geom_line(aes(time, amp), color = '#186356') +
+  scale_x_continuous(name = 'Time (s)') +
+  scale_y_continuous('Amplitude (kU)') +
+  theme_pubr() +
+  theme(axis.text.y = element_text(angle = 90, vjust = 1, hjust=.5))
+
+print(g1)
+
+###
+g2 = ggplot(data.frame(amp = wav2@left,
+                       time = seq_along(wav2@left)/10^5)) +
+  geom_line(aes(time, amp), color = '#186356') +
+  scale_x_continuous(name = 'Time (s)') +
+  scale_y_continuous('') +
+  theme_pubr() +
+  theme(axis.text.y = element_text(angle = 90, vjust = 1, hjust=.5),
+        plot.margin = margin(, .4, , , "cm"))
+
+print(g2)
+
+###
+g3 = ggplot(data.frame(amp = wav3@left,
+                       time = seq_along(wav3@left)/10^5)) +
+  geom_line(aes(time, amp), color = '#186356') +
+  scale_x_continuous(name = 'Time (s)') +
+  scale_y_continuous('Amplitude (kU)') +
+  geom_segment(aes(x = 0.002, y=0, xend = 0.002, yend = 34000), linewidth = 1, linetype = 2) +
+  geom_segment(aes(x = 0.0078, y=0, xend = 0.0078, yend = 34000), linewidth = 1, linetype = 2) +
+  geom_segment(aes(x = 0.0173, y=0, xend = 0.0173, yend = 34000), linewidth = 1, linetype = 2) +
+  geom_segment(aes(x = 0.028, y=0, xend = 0.028, yend = 34000), linewidth = 1, linetype = 2) +
+  geom_segment(aes(x = 0.0351, y=0, xend = 0.0351, yend = 34000), linewidth = 1, linetype = 2) +
+  geom_segment(aes(x=0.002, y=34000, xend=0.0078, yend =34000), size = 1, arrow = arrow(length = unit(0.3, "cm"))) +
+  geom_segment(aes(x=0.008, y=34000, xend=0.0173, yend =34000), size = 1, arrow = arrow(length = unit(0.3, "cm"))) +
+  geom_segment(aes(x=0.0175, y=34000, xend=0.028, yend =34000), size = 1, arrow = arrow(length = unit(0.3, "cm"))) +
+  geom_segment(aes(x=0.0282, y=34000, xend=0.0351, yend =34000), size = 1, arrow = arrow(length = unit(0.3, "cm"))) +
+  geom_segment(aes(x=0.0353, y=34000, xend=0.040, yend =34000), size = 1) +
+  geom_segment(aes(x=0.04, y=34000, xend=0.043, yend =34000), size = 1, linetype = 2) +
+  geom_segment(aes(x=0.043, y=34000, xend=0.044, yend =34000), size =1, arrow = arrow(length = unit(0.3, "cm"))) +
+  annotate(geom = 'label', x=0.005, y = 31000, label = 'PPD 1', label.size = NA) +
+  annotate(geom = 'label', x=0.013, y = 31000, label = 'PPD 2', label.size = NA) +
+  annotate(geom = 'label', x=0.0225, y = 31000, label = 'PPD 3', label.size = NA) +
+  annotate(geom = 'label', x=0.0315, y = 31000, label = 'PPD 4', label.size = NA) +
+  annotate(geom = 'label', x=0.040, y = 31000, label = 'PPD 5', label.size = NA) +
+  theme_pubr() +
+  theme(axis.text.y = element_text(angle = 90, vjust = 1, hjust=.5))
+
+print(g3)
+
+pp = ggarrange(
+  ggarrange(g1,
+            g2,
+            nrow = 1,
+            ncol = 2,
+            align = 'hv',
+            labels = c('A','B')),
+  g3,
+  nrow = 2,
+  labels = c('','C'))
+
+print(pp)
+
+###SAVE
+ggsave(filename = 'wave.jpg',  #filename = 'wave.svg'
+       pp,
+       path = 'C:/Users/G00399072/OneDrive - Atlantic TU/Desktop/call_paper/graphs',
+       width = 240, height = 180, units = "mm",
+       dpi = 600)
+
+#END
+
+###SPECTROGRAM
+
+specs = readWave('C:/Users/G00399072/OneDrive - Atlantic TU/Desktop/call_paper/ZOOM0018.WAV.wav',
+                 from = 14.8, to = 17, units = 'seconds') %>% channel('left')
+## first layer
+## using geom_tile ##
+ss = ggspectro(specs, f = 96000) +
+  geom_raster(aes(fill = amplitude), interpolate = TRUE) +
+  geom_hline(yintercept = 1, linetype="dashed", 
+             color = '#FAAB36', linewidth=1) +
+  scale_y_continuous(breaks = c(12,24,36,48),
+                     limits = c(0,48),
+                     expand = c(0,0)) +
+  scale_x_continuous(breaks = c(0,.5,1,1.5,2),
+                     expand = c(0,0)) +
+  scale_fill_gradient2(name="Amplitude (dB)",
+                       breaks = c(-140,0),
+                       low = "white",
+                       mid = '#186356') +
+  theme_pubr() +
+  theme(legend.position = 'bottom',
+        legend.key.size = unit(0.4, "cm"))
+
+print(ss)
+
+specc = readWave('C:/Users/G00399072/OneDrive - Atlantic TU/Desktop/call_paper/ZOOM0018.WAV_cleaned.wav',
+                 from = 14.8, to = 17, units = 'seconds') %>% channel('left')
+
+sc = ggspectro(specc, f = 96000) +
+  geom_raster(aes(fill = amplitude), interpolate = TRUE) +
+  geom_hline(yintercept = 1, linetype="dashed", 
+             color = '#FAAB36', linewidth=1) +
+  scale_y_continuous(breaks = c(12,24,36,48),
+                     limits = c(0,48),
+                     expand = c(0,0)) +
+  scale_x_continuous(breaks = c(0,.5,1,1.5,2),
+                     expand = c(0,0)) +
+  scale_fill_gradient2(name="Amplitude (dB)",
+                       breaks = c(-200,0),
+                       low = "white",
+                       mid = '#186356') +
+  theme_pubr() +
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        legend.position = 'bottom',
+        legend.key.size = unit(0.4, "cm")) 
+
+print(sc)
+
+#END
